3cb195b435a13c39fc6c51f5e6a1404c2bbc25fb
StaticCloud <diegomarrs@gmail.com>
2023-09-10 20:51:36 -0700

    added image component to features section

--- app/components/FeaturesSection.tsx
+++ app/components/FeaturesSection.tsx
@@ -1,31 +1,39 @@
+import Image from "next/image";
+
 type CardInfo = {
     title: string,
-    text: string
+    text: string,
+    image: string
 }
 
 const features: CardInfo[] = [
     {
         title: "Fast and Simple",
-        text: "Websites are programmed and optimized with performance at the forefront, ensuring fast websites without the bloat that comes with traditional website builders."
+        text: "Websites are programmed and optimized with performance at the forefront, ensuring fast websites without the bloat that comes with traditional website builders.",
+        image: ""
     },
     {
         title: "Responsive",
-        text: "Websites are optimized for a wide variety of screen sizes, ensuring that your website will look great on any device."
+        text: "Websites are optimized for a wide variety of screen sizes, ensuring that your website will look great on any device.",
+        image: ""
     },
     {
         title: "Creative Freedom",
-        text: "Customers have the final say on the look of their website, no more spending hours searching for the perfect template."
+        text: "Customers have the final say on the look of their website, no more spending hours searching for the perfect template.",
+        image: ""
     },
     {
         title: "SEO Friendly",
-        text: "We leverage best SEO practices and optimizations such as server-side rendering, giving your website a much stronger site ranking on search engines."
+        text: "We leverage best SEO practices and optimizations such as server-side rendering, giving your website a much stronger site ranking on search engines.",
+        image: ""
     }
 ]
 
 
-function Card({ title, text }: CardInfo) {
+function Card({ title, text, image }: CardInfo) {
     return (
         <div>
+            <Image src={image} alt={title}/>
             <h1>{title}</h1>
             <p>{text}</p>
         </div>
@@ -36,7 +44,7 @@ export default function FeaturesSection() {
     return (
         <section>
             <h1>This is where features will go</h1>
-            {features.map(feature => Card({ title: feature.title, text: feature.text }))}
+            {features.map(feature => Card({ title: feature.title, text: feature.text, image: feature.image }))}
         </section>
     )
 }
