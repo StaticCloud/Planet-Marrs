45b8ea8722616d91181024f29c3c41965a2da4a1
StaticCloud <diegomarrs@gmail.com>
2023-09-21 12:12:22 -0700

    added proper nav and logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,26 @@
 "use client"
 
 import { archivo } from "../fonts";
+import Logo from '@/public/images/logo.png';
+import Image from "next/image";
+import Link from "next/link";
 import styles from "./styles/Header.module.css";
 
 export default function Header() {
     return (
         <header className={`${archivo.className} ${styles.header}`}>
-            <h1>Planet Marrs</h1>
+            <Image src={Logo} width={'60'} height={'60'} alt="logo"/>
+            <ul>
+                <li>
+                    <Link href="#landing">HOME</Link>
+                </li>
+                <li>
+                    <Link href="#features">FEATURES</Link>
+                </li>
+                <li>
+                    <Link href="#pricing">PRICING</Link>
+                </li>
+            </ul>
         </header>
     );
 }
