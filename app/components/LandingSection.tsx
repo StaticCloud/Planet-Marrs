1b5ce0649b178cea4e71a84900c5ae8d100964b3
DMarrs <diegomarrs@gmail.com>
2023-09-22 01:14:16 -0700

    added images to home section

--- app/components/LandingSection.tsx
+++ app/components/LandingSection.tsx
@@ -1,4 +1,9 @@
 import styles from "./styles/LandingSection.module.css"
+import Image from "next/image";
+
+import SiteOne from '@/public/images/site-one.png'
+import SiteTwo from '@/public/images/site-two.png'
+import SiteThree from '@/public/images/site-three.png'
 
 export default function LandingSection() {
     return (
@@ -7,8 +12,10 @@ export default function LandingSection() {
                 <h1>Professional, lightweight, and fast websites.</h1>
                 <p>Planet Marrs provides customers with websites that are coded from the ground up, responsive, and free from the frustration, visual limitations, or bloat of website builders.</p>
             </div>
-            <div>
-                <h1>An image will go here, eventually.</h1>
+            <div className={styles.landingSites}>
+                <Image src={SiteOne} alt="sample-site-one"/>
+                <Image src={SiteTwo} alt="sample-site-two"/>
+                <Image src={SiteThree} alt="sample-site-three"/>
             </div>
         </section>
     )
