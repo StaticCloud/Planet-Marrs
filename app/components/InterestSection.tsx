43b419af239d28ce1740207bbd63d8032a2a5329
StaticCloud <diegomarrs@gmail.com>
2023-09-29 14:05:32 -0700

    added sharp package and fixed eslint errors

--- app/components/InterestSection.tsx
+++ app/components/InterestSection.tsx
@@ -6,7 +6,7 @@ export default function InterestSection() {
         <section className={style.interestSection} id="interest">
             <h1 className='sectionTitle'>Interested?</h1>
             <div className={style.interestContent}>
-                <p>Interested in hiring Planet Marrs to build your website? Maybe you're on the fence and wish to keep looking? No worries! Feel free to fill out the interest form below if you want to keep us in your radar, or if you're ready to chat and build something great!</p>
+                <p>Interested in hiring Planet Marrs to build your website? Maybe you&apos;re on the fence and wish to keep looking? No worries! Feel free to fill out the interest form below if you want to keep us in your radar, or if you&apos;re ready to chat and build something great!</p>
                 <Link className="button" target="_blank" href="https://forms.gle/4FeqN2b1hD9oCLLS8">INTEREST FORM</Link>
             </div>
         </section>
