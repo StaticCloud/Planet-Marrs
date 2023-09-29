01be9847b8d75b67c77f8d3f2c9822bd3cfa3db8
dmarrs-qelZcl <dmarrs@instructors.2u.com>
2023-09-28 19:27:07 -0700

    state functional animation for hamburger menu

--- app/components/HamburgerMenu.tsx
+++ app/components/HamburgerMenu.tsx
@@ -1,11 +1,21 @@
 import { FC } from "react";
+import { useState } from "react";
 import styles from './styles/HamburgerMenu.module.css';
 
 const HamburgerMenu:FC = () => {
+    const [visible, setVisible] = useState(false);
+
+    const OpenMobileMenu = () => {
+        setVisible(!visible);
+    }
+
     return (
         <nav className={styles.hamburgerMenu}>
-            <button className={styles.hamburgerButton}>
-                <div className={styles.hamburgerIcon}/>
+            <button className={styles.hamburgerButton} onClick={() => OpenMobileMenu()}>
+                <div className={`
+                    ${styles.hamburgerIcon}
+                    ${visible ? styles.hamburgerIconOpen : ''}
+                `}/>
             </button>
             <ul>
 
