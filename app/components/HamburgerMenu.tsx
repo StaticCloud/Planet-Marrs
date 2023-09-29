36e231dab224c213ec2fb3bf8c1af96e029b7b52
StaticCloud <diegomarrs@gmail.com>
2023-09-29 13:50:06 -0700

    functional mobile menu

--- app/components/HamburgerMenu.tsx
+++ app/components/HamburgerMenu.tsx
@@ -1,24 +1,35 @@
 import { FC } from "react";
 import { useState } from "react";
 import styles from './styles/HamburgerMenu.module.css';
+import Link from "next/link";
 
-const HamburgerMenu:FC = () => {
+const HamburgerMenu: FC = () => {
     const [visible, setVisible] = useState(false);
 
-    const OpenMobileMenu = () => {
+    const ToggleMobileMenu = () => {
         setVisible(!visible);
     }
 
     return (
         <nav className={styles.hamburgerMenu}>
-            <button className={styles.hamburgerButton} onClick={() => OpenMobileMenu()}>
+            <button className={styles.hamburgerButton} onClick={() => ToggleMobileMenu()}>
                 <div className={`
                     ${styles.hamburgerIcon}
                     ${visible ? styles.hamburgerIconOpen : ''}
-                `}/>
+                `} />
             </button>
-            <ul>
-
+            <ul className={`
+                    ${visible ? styles.mobileMenuActive : styles.mobileMenu}
+                `} onClick={() => ToggleMobileMenu()}>
+                <li>
+                    <Link href="#landing">HOME</Link>
+                </li>
+                <li>
+                    <Link href="#features">FEATURES</Link>
+                </li>
+                <li>
+                    <Link href="#pricing">PRICING</Link>
+                </li>
             </ul>
         </nav>
     );
