9d8466b599332642c8c544989ee0cbd218d2dba4
StaticCloud <diegomarrs@gmail.com>
2023-10-04 19:49:19 -0700

    added metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import type { Metadata } from "next";
 import LandingSection from "./components/LandingSection"
 import FeaturesSection from "./components/FeaturesSection"
 import PricingSection from "./components/PricingSection"
 import InterestSection from "./components/InterestSection"
 import ContactSection from "./components/ContactSection"
+
+export const metadata: Metadata = {
+  title: "Planet Marrs",
+  description: "Professional, lightweight, responsive, and fast websites tailored to your needs, whether it's a portfolio or a professional website for your business."
+}
 
 export default function Home() {
   return (
